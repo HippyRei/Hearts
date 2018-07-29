19e5dcccc2754b6cffdde5b8acda485a6e23a46a
Reilly Mulligan <reillymull@gmail.com>
2018-07-28 18:48:36 -0700

    Initial commit

---
+++ Hearts/HeartsMain.cs
@@ -0,0 +1,15 @@
+﻿namespace Hearts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class HeartsMain
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
